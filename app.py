6d485bd6f2d6bf295fc921e5d92f21d123243a95
malekoa <azadeganmalek@gmail.com>
2023-12-14 12:32:39 -0500

    removed pycache, added error handling, updated homepage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""Flask app for FizzBuzz"""
+"""Flask app for FizzBuzzAAS"""
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -27,8 +27,14 @@ def fizzbuzz():
     `buzz`: Buzz number. Default is 5.
     """
     fizz = int(request.args.get('fizz', 3))
+    if fizz < 1:
+        return {'Error': 'Fizz must be greater than 0.'}, 400
     buzz = int(request.args.get('buzz', 5))
+    if buzz < 1:
+        return {'Error': 'Buzz must be greater than 0.'}, 400
     limit = int(request.args.get('limit', 100))
+    if limit < 1:
+        return {'Error': 'Limit must be greater than 0.'}, 400
     if limit > 1000:
         return {'Error': 'Limit is too large. Max is 1000.'}, 400
     fizzbuzz_list = []
@@ -52,8 +58,14 @@ def fizzbuzz_prime():
     `buzz`: Buzz number. Default is 5.
     """
     fizz = int(request.args.get('fizz', 3))
+    if fizz < 1:
+        return {'Error': 'Fizz must be greater than 0.'}, 400
     buzz = int(request.args.get('buzz', 5))
+    if buzz < 1:
+        return {'Error': 'Buzz must be greater than 0.'}, 400
     limit = int(request.args.get('limit', 100))
+    if limit < 1:
+        return {'Error': 'Limit must be greater than 0.'}, 400
     if limit > 1000:
         return {'Error': 'Limit is too large. Max is 1000.'}, 400
     fizzbuzz_list = []
@@ -70,28 +82,31 @@ def fizzbuzz_prime():
             fizzbuzz_list.append(i)
     return {'FizzBuzz': fizzbuzz_list}, 200
 
-# POST /fizzbuzz that takes in a JSON body with the following fields:
-# `targets`: A list of string,int tuples. The string is the target word, and the int is the number to replace.
-# `limit`: The max number to count to. Default is 100.
-# Example: {"targets": [["Fizz", 3], ["Buzz", 5]], "limit": 100}
 @app.route('/fizzbuzz', methods=['POST'])
 def fizzbuzz_post():
     """
     POST /fizzbuzz that takes in a JSON body with the following fields:
-    `targets`: A list of string,int tuples. The string is the target word, and the int is the number to replace.
+    `targets`: A list of string,int tuples. The string is the target word, 
+    and the int is the number to replace.
     `limit`: The max number to count to. Default is 100.
-    Example: {"targets": [["Fizz", 3], ["Buzz", 5]], "limit": 100}
+    Example: {"targets": [["Fizz", 3], ["Buzz", 5]], "limit": 100}.
     """
     data = request.get_json()
     targets = data.get('targets')
     if not targets:
         return {'Error': 'Targets is required.'}, 400
-    targets = [(tup[0], int(tup[1])) for tup in targets]
-
+    try:
+        targets = [(tup[0], int(tup[1])) for tup in targets]
+    except IndexError:
+        return {'Error': 'Ill-formed targets array.'}, 400
+    for target, num in targets:
+        if num < 1:
+            return {'Error': f'Target numbers must be greater than 0. Error at: [{target}, {num}]'}, 400
     limit = data.get('limit', 100)
     if not limit:
         return {'Error': 'Limit is required.'}, 400
-
+    if limit < 1:
+        return {'Error': 'Limit must be greater than 0.'}, 400
     if limit > 1000:
         return {'Error': 'Limit is too large. Max is 1000.'}, 400
     fizzbuzz_list = []
